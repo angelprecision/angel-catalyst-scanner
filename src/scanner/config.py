e2dd91bf2b5d1b20f639162338497fbfe53e5497
angelprecision <g44644rwz9@privaterelay.appleid.com>
2026-02-04 21:54:28 -0800

    Update config.py

--- src/scanner/config.py
+++ src/scanner/config.py
@@ -1,1 +1,8 @@
+import os
+
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY", "").strip()
+MARKETDATA_API_KEY = os.getenv("MARKETDATA_API_KEY", "").strip()
+
+DAYS_AHEAD = int(os.getenv("DAYS_AHEAD", "14"))
+MAX_TICKERS = int(os.getenv("MAX_TICKERS", "40"))
 
